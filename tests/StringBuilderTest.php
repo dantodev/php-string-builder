bab9313e49717ca1baefd0a28cda2551bc7ba0bc
Daniel Kahl <kontakt@daniel-kahl.de>
2016-03-17 19:22:28 +0100

    test travis ci fail

--- tests/StringBuilderTest.php
+++ tests/StringBuilderTest.php
@@ -49,4 +49,9 @@ class StringBuilderTest extends \PHPUnit_Framework_TestCase
     $this->assertEquals($sb->build(), 'a2a3a1');
   }
 
+  public function testFail()
+  {
+    $this->assertFalse(true);
+  }
+
 }
