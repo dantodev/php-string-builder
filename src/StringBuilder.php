f1eeeeda0d287372a98d0b7129c612c7c48cc318
Daniel Kahl <kontakt@daniel-kahl.de>
2016-03-17 18:34:10 +0100

    * add methods and tests

--- src/StringBuilder.php
+++ src/StringBuilder.php
@@ -3,6 +3,49 @@
 class StringBuilder
 {
 
+  private $_start = '';
+  private $_end = '';
+  private $_string = '';
 
+  public function __construct($start = '', $end = '')
+  {
+    $this->_start = (string) $start;
+    $this->_end = (string) $end;
+  }
+
+  public function append($part, array $data = [])
+  {
+    $this->_string = $this->_string . (empty($data) ? (string) $part : $this->_template((string) $part, $data));
+  }
+
+  public function prepend($part, array $data = [])
+  {
+    $this->_string = (empty($data) ? (string) $part : $this->_template((string) $part, $data)) . $this->_string;
+  }
+
+  private function _template($string, array $data)
+  {
+    return call_user_func_array('sprintf', array_merge([$string], $data));
+  }
+
+  public function startWith($start)
+  {
+    $this->_start = $start;
+  }
+
+  public function endWith($end)
+  {
+    $this->_end = $end;
+  }
+
+  public function build()
+  {
+    return $this->_start . $this->_string . $this->_end;
+  }
+
+  public function __toString()
+  {
+    return $this->build();
+  }
 
 }
--- tests/StringBuilderTest.php
+++ tests/StringBuilderTest.php
@@ -2,16 +2,51 @@
 
 class StringBuilderTest extends \PHPUnit_Framework_TestCase
 {
-  private $_sb;
 
-  protected function setUp()
+  public function testConstructToString()
   {
-    $this->_sb = new StringBuilder();
+    $sb = new StringBuilder('a1', 'a2');
+    $this->assertEquals($sb->__toString(), 'a1a2');
   }
 
-  public function testStringBuilder()
+  public function testAppend()
   {
-    $this->assertEquals(get_class($this->_sb), StringBuilder::class);
+    $sb = new StringBuilder();
+    $sb->append('a1');
+    $sb->append('a2');
+    $this->assertEquals($sb->build(), 'a1a2');
+  }
+
+  public function testPrepend()
+  {
+    $sb = new StringBuilder();
+    $sb->append('a1');
+    $sb->prepend('a2');
+    $this->assertEquals($sb->build(), 'a2a1');
+  }
+
+  public function testStartWithEndWith()
+  {
+    $sb = new StringBuilder();
+    $sb->startWith('a1');
+    $sb->endWith('a2');
+    $this->assertEquals($sb->build(), 'a1a2');
+  }
+
+  public function testAppendTemplateString()
+  {
+    $sb = new StringBuilder();
+    $sb->append('a1');
+    $sb->append('a2%s', ['a3']);
+    $this->assertEquals($sb->build(), 'a1a2a3');
+  }
+
+  public function testPrependTemplateString()
+  {
+    $sb = new StringBuilder();
+    $sb->append('a1');
+    $sb->prepend('a2%s', ['a3']);
+    $this->assertEquals($sb->build(), 'a2a3a1');
   }
 
 }
