ef2f51315f74b6e942224342f89b67c329679109
daniel <kontakt@daniel-kahl.de>
2016-05-20 16:54:39 +0200

    * add PSR-2 compatibility #1

--- src/StringBuilder.php
+++ src/StringBuilder.php
@@ -3,109 +3,109 @@
 class StringBuilder
 {
 
-  private $_start = '';
-  private $_end = '';
-  private $_string = '';
+    private $_start = '';
+    private $_end = '';
+    private $_string = '';
 
-  /**
-   * StringBuilder constructor.
-   * @param string $start
-   * @param string $end
-   */
-  public function __construct($start = '', $end = '')
-  {
-    $this->_start = (string) $start;
-    $this->_end = (string) $end;
-  }
+    /**
+     * StringBuilder constructor.
+     * @param string $start
+     * @param string $end
+     */
+    public function __construct($start = '', $end = '')
+    {
+        $this->_start = (string)$start;
+        $this->_end = (string)$end;
+    }
 
-  /**
-   * @param $part
-   * @param array $data
-   * @return $this
-   */
-  public function append($part, array $data = [])
-  {
-    $this->_string = $this->_string . (empty($data) ? (string) $part : self::template((string) $part, $data));
-    return $this;
-  }
+    /**
+     * @param $part
+     * @param array $data
+     * @return $this
+     */
+    public function append($part, array $data = [])
+    {
+        $this->_string = $this->_string . (empty($data) ? (string)$part : self::template((string)$part, $data));
+        return $this;
+    }
 
-  /**
-   * @param $part
-   * @param array $data
-   * @return $this
-   */
-  public function prepend($part, array $data = [])
-  {
-    $this->_string = (empty($data) ? (string) $part : self::template((string) $part, $data)) . $this->_string;
-    return $this;
-  }
+    /**
+     * @param $part
+     * @param array $data
+     * @return $this
+     */
+    public function prepend($part, array $data = [])
+    {
+        $this->_string = (empty($data) ? (string)$part : self::template((string)$part, $data)) . $this->_string;
+        return $this;
+    }
 
-  /**
-   * @param $string
-   * @param array $data
-   * @return mixed
-   */
-  public static function template($string, array $data)
-  {
-    return call_user_func_array('sprintf', array_merge([$string], $data));
-  }
+    /**
+     * @param $string
+     * @param array $data
+     * @return mixed
+     */
+    public static function template($string, array $data)
+    {
+        return call_user_func_array('sprintf', array_merge([$string], $data));
+    }
 
-  /**
-   * @param $start
-   * @return $this
-   */
-  public function startWith($start)
-  {
-    $this->_start = (string) $start;
-    return $this;
-  }
+    /**
+     * @param $start
+     * @return $this
+     */
+    public function startWith($start)
+    {
+        $this->_start = (string)$start;
+        return $this;
+    }
 
-  /**
-   * @param $end
-   * @return $this
-   */
-  public function endWith($end)
-  {
-    $this->_end = (string) $end;
-    return $this;
-  }
+    /**
+     * @param $end
+     * @return $this
+     */
+    public function endWith($end)
+    {
+        $this->_end = (string)$end;
+        return $this;
+    }
 
-  /**
-   * @return string
-   */
-  public function build()
-  {
-    return $this->_start . $this->_string . $this->_end;
-  }
+    /**
+     * @return string
+     */
+    public function build()
+    {
+        return $this->_start . $this->_string . $this->_end;
+    }
 
-  /**
-   * @return string
-   */
-  public function __toString()
-  {
-    return $this->build();
-  }
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->build();
+    }
 
-  /**
-   * @param string $part
-   * @param string $find
-   * @param string $replace
-   * @return $this
-   */
-  public function prependReplace($part, $find, $replace)
-  {
-    return $this->prepend(str_replace((string) $find, (string) $replace, (string)$part));
-  }
+    /**
+     * @param string $part
+     * @param string $find
+     * @param string $replace
+     * @return $this
+     */
+    public function prependReplace($part, $find, $replace)
+    {
+        return $this->prepend(str_replace((string)$find, (string)$replace, (string)$part));
+    }
 
-  /**
-   * @param string $part
-   * @param string $find
-   * @param string $replace
-   * @return $this
-   */
-  public function appendReplace($part, $find, $replace)
-  {
-    return $this->append(str_replace((string) $find, (string) $replace, (string) $part));
-  }
+    /**
+     * @param string $part
+     * @param string $find
+     * @param string $replace
+     * @return $this
+     */
+    public function appendReplace($part, $find, $replace)
+    {
+        return $this->append(str_replace((string)$find, (string)$replace, (string)$part));
+    }
 
 }
--- tests/StringBuilderTest.php
+++ tests/StringBuilderTest.php
@@ -3,66 +3,66 @@
 class StringBuilderTest extends \PHPUnit_Framework_TestCase
 {
 
-  public function testConstructToString()
-  {
-    $sb = new StringBuilder('a1', 'a2');
-    $this->assertEquals('a1a2', $sb->__toString());
-  }
+    public function testConstructToString()
+    {
+        $sb = new StringBuilder('a1', 'a2');
+        $this->assertEquals('a1a2', $sb->__toString());
+    }
 
-  public function testAppend()
-  {
-    $sb = new StringBuilder();
-    $sb->append('a1');
-    $sb->append('a2');
-    $this->assertEquals('a1a2', $sb->build());
-  }
+    public function testAppend()
+    {
+        $sb = new StringBuilder();
+        $sb->append('a1');
+        $sb->append('a2');
+        $this->assertEquals('a1a2', $sb->build());
+    }
 
-  public function testPrepend()
-  {
-    $sb = new StringBuilder();
-    $sb->append('a1');
-    $sb->prepend('a2');
-    $this->assertEquals('a2a1', $sb->build());
-  }
+    public function testPrepend()
+    {
+        $sb = new StringBuilder();
+        $sb->append('a1');
+        $sb->prepend('a2');
+        $this->assertEquals('a2a1', $sb->build());
+    }
 
-  public function testStartWithEndWith()
-  {
-    $sb = new StringBuilder();
-    $sb->startWith('a1');
-    $sb->endWith('a2');
-    $this->assertEquals('a1a2', $sb->build());
-  }
+    public function testStartWithEndWith()
+    {
+        $sb = new StringBuilder();
+        $sb->startWith('a1');
+        $sb->endWith('a2');
+        $this->assertEquals('a1a2', $sb->build());
+    }
 
-  public function testAppendTemplateString()
-  {
-    $sb = new StringBuilder();
-    $sb->append('a1');
-    $sb->append('a2%s', ['a3']);
-    $this->assertEquals('a1a2a3', $sb->build());
-  }
+    public function testAppendTemplateString()
+    {
+        $sb = new StringBuilder();
+        $sb->append('a1');
+        $sb->append('a2%s', ['a3']);
+        $this->assertEquals('a1a2a3', $sb->build());
+    }
 
-  public function testPrependTemplateString()
-  {
-    $sb = new StringBuilder();
-    $sb->append('a1');
-    $sb->prepend('a2%s', ['a3']);
-    $this->assertEquals('a2a3a1', $sb->build());
-  }
+    public function testPrependTemplateString()
+    {
+        $sb = new StringBuilder();
+        $sb->append('a1');
+        $sb->prepend('a2%s', ['a3']);
+        $this->assertEquals('a2a3a1', $sb->build());
+    }
 
-  public function testPrependReplace()
-  {
-    $sb = new StringBuilder();
-    $sb->append('a1');
-    $sb->prependReplace('a2', 'a', 'b');
-    $this->assertEquals('b2a1', $sb->build());
-  }
+    public function testPrependReplace()
+    {
+        $sb = new StringBuilder();
+        $sb->append('a1');
+        $sb->prependReplace('a2', 'a', 'b');
+        $this->assertEquals('b2a1', $sb->build());
+    }
 
-  public function testAppendReplace()
-  {
-    $sb = new StringBuilder();
-    $sb->append('a1');
-    $sb->appendReplace('a2', 'a', 'b');
-    $this->assertEquals('a1b2', $sb->build());
-  }
+    public function testAppendReplace()
+    {
+        $sb = new StringBuilder();
+        $sb->append('a1');
+        $sb->appendReplace('a2', 'a', 'b');
+        $this->assertEquals('a1b2', $sb->build());
+    }
 
 }
